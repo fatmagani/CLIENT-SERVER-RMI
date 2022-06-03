4cdc965e625840803b5c1a6bf746a56038118f2b
gani fatima <83297592+fatmagani@users.noreply.github.com>
2022-06-03 23:11:57 +0200

    Add files via upload

--- server/src/rmi/aserveur.java
+++ server/src/rmi/aserveur.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
 //import sun.java2d.Disposer;
 
 public class aserveur extends UnicastRemoteObject implements servinterface {
-double scoree;
+double numb;
 double choice2;
 
 
@@ -33,21 +33,21 @@ double choice2;
         }
     }
     
-    public double game(String c) throws RemoteException, ServerNotActiveException {
+    public double game(double number,String c) throws RemoteException, ServerNotActiveException {
         
         double result = 0;
         
         try {
             System.out.println("sise :"+Serv.it.size());
-           
-            for (int i = 0; i < Serv.it.size(); i++) {
-        
-                double ch= Double.parseDouble(Serv.it.get(i).toString());
+           System.out.println("NUMBER    "+number);
+            
+        numb=number;
+                double ch= Double.parseDouble(Serv.it.get(Serv.it.size()-1).toString());
                 System.out.println("choix1    "+ch);
-                double chh= Double.parseDouble(Serv.it.get(i+1).toString());
+                double chh= Double.parseDouble(Serv.it2.get(Serv.it2.size()-1).toString());
                 System.out.println("choix2     "+chh);
-                
-        choice2=chh;
+                System.out.println("NUMBER    "+number);
+         
                 if(ch== chh)
                 {  
                     result = 0;}
@@ -78,26 +78,39 @@ double choice2;
                 { result = 2;
                 }
              
-            }
+            
         }catch(Exception e){
         }
         return result;
         
     } 
       @Override
-    public double choice2(String c) throws RemoteException, ServerNotActiveException {
+    public double choice22(double number,String c) throws RemoteException, ServerNotActiveException {
         
         double choic2 = 0;
         
         try {
-            for (int i = 0; i < Serv.it.size(); i++) {    
-     choic2= Double.parseDouble(Serv.it.get(i+1).toString());}
+            choic2=Double.parseDouble(Serv.it.get(Serv.it.size()-1).toString());
         }
         catch(Exception e){
         }
         
-        return choice2;
+        return choic2;
     }
+       @Override
+    public double choice12(double number,String c) throws RemoteException, ServerNotActiveException {
+        
+        double choic2 = 0;
+        
+        try {
+            choic2=Double.parseDouble(Serv.it2.get(Serv.it2.size()-1).toString());
+        }
+        catch(Exception e){
+        }
+        
+        return choic2;
+    }
+
 
     
     
@@ -105,16 +118,43 @@ double choice2;
    
 
     @Override
-    public double choice(double score,double a, String c) throws RemoteException, ServerNotActiveException {
-        scoree=score;
+    public double choice1(double number,double a, String c) throws RemoteException, ServerNotActiveException {
+        numb=number;
         double choix = 0;
         
         try {
            
             choix = a;
+          
             Serv.it.add(choix);
-            System.out.println("sise00 :"+Serv.it.size());
+             
+            System.out.println("NUMBER    "+numb);
+            System.out.println("sise00 :"+Serv.it.size());}
+            
+            
+        
+        catch(Exception e){
         }
+        
+        return choix;
+    }
+    
+     @Override
+    public double choice2(double number,double a, String c) throws RemoteException, ServerNotActiveException {
+        numb=number;
+        double choix = 0;
+        
+        try {
+           
+            choix = a;
+           
+            
+                Serv.it2.add(choix);
+               
+            System.out.println("sise01 :"+Serv.it2.size());
+        }
+            
+        
         catch(Exception e){
         }
         
--- server/src/rmi/servinterface.java
+++ server/src/rmi/servinterface.java
@@ -6,7 +6,9 @@ import java.rmi.server.ServerNotActiveException;
 
 public interface servinterface extends Remote {
    // public double inter (double a) throws RemoteException,ServerNotActiveException;
-    public double game (String c) throws RemoteException,ServerNotActiveException;
-    public double choice(double score,double a,String c) throws RemoteException,ServerNotActiveException;
-    public double choice2(String c)throws RemoteException,ServerNotActiveException;
+    public double game (double number,String c) throws RemoteException,ServerNotActiveException;
+    public double choice1(double number,double a,String c) throws RemoteException,ServerNotActiveException;
+    public double choice2(double number,double a,String c) throws RemoteException,ServerNotActiveException;
+    public double choice12(double number,String c)throws RemoteException,ServerNotActiveException;
+    public double choice22(double number,String c)throws RemoteException,ServerNotActiveException;
 }
