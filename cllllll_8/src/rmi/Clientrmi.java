bd2d114dd6b7483b82222edaba6f4ed6beece9a2
gani fatima <83297592+fatmagani@users.noreply.github.com>
2022-06-04 12:41:55 +0200

    Add files via upload

---
+++ cllllll_8/src/rmi/Clientrmi.java
@@ -0,0 +1,13 @@
+package rmi;
+
+public class Clientrmi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        connection a = new connection();
+        a.setVisible(true);
+    }
+}
